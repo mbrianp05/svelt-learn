26f2d86d98a3cc2ddff2985710a571f7b6e95634
Brian <mbrianp05@gmail.com>
2022-06-14 18:59:06 -0400

    First Commit

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script>
+  let name = 'Brian'
+
+  const handleClick = () => {
+    alert('Hello World')
+  }
+</script>
+
+<main>
+  <h1>{name}</h1>
+  <button on:click={handleClick}>Click it!</button>
+</main>
